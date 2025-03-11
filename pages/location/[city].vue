287ff4a4c5a54ae650e2e40a91c8fe7c66f44214
YaroslavGudkov <yaroslavhudkov@gmail.com>
2025-03-11 18:42:52 +0300

    feature: removed back button

--- pages/location/[city].vue
+++ pages/location/[city].vue
@@ -1,43 +1,29 @@
 <template>
   <div class="location-page">
-    <div>
-      <button @click="goBack()">back</button>
-    </div>
-    <h2>Weather for {{ location.city }} {{ location.country }}</h2>
-      <WeatherViewer v-if="latitude && longitude"/>
-    <!-- <WeatherViewer v-if="latitude" :location="location" /> -->
+    <WeatherViewer v-if="latitude && longitude"/>
   </div>
 </template>
 
 <script setup>
 import { onMounted } from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { useRoute } from "vue-router";
 import { useLocationStore } from "~/stores/location";
 import WeatherViewer from "~/components/WeatherViewer.vue";
 
 const locationStore = useLocationStore()
 
 const route = useRoute()
-
-const city = route.params.city
-const country = route.query.country
 const latitude = route.query.lat
 const longitude = route.query.lon
 
-const router = useRouter()
-function goBack() {
-  router.back()
-}
-console.log({country})
-
+const city = route.params.city
+const country = route.query.country
 const location = {
   city,
   country,
   latitude,
   longitude,
 };
-console.log({location})
-console.log({q:route.query})
 locationStore.setLocation(location)
 </script>
 
